b4c39c4096c4fc6392a5e16f3303a07a5fb5f22f
fanwenjie <fanwj@zhejianglab.org>
2026-06-12 10:35:19 +0800

    add training_scipts

---
+++ training_scripts/geo-train-cpt.sh
@@ -0,0 +1,39 @@
+# Based on the open-source framework ms-swift 
+run_cmd="megatron pt \
+    --model $MODEL_DIR \
+    --save_safetensors true \
+    --add_version false \
+    --loss_scale all \
+    --finetune true \
+    --model_type $MODEL_TYPE \
+    --template $TEMPLATE \
+    --cached_dataset $DATA_DIR \
+    --add_non_thinking_prefix false \
+    --truncation_strategy right \
+    --use_chat_template false \
+    --load_from_cache_file true \
+    --tensor_model_parallel_size $TP \
+    --pipeline-model-parallel-size $PP \
+    --sequence_parallel true \
+    --micro_batch_size 1 \
+    --global_batch_size $GLOBAL_BATCH_SIZE \
+    --recompute_granularity full \
+    --recompute_method uniform \
+    --recompute_num_layers 1 \
+    --cross_entropy_loss_fusion true \
+    --lr $LR \
+    --lr_warmup_fraction 0.05 \
+    --min_lr $MIN_LR \
+    --num_train_epochs $EPOCH \
+    --output_dir $OUTPUT_DIR \
+    --save_steps $SAVE_STEPS \
+    --max_length $SEQ_LEN \
+    --group_by_length false \
+    --dataloader_num_workers 8 \
+    --dataset_num_proc 16  \
+    --attention_backend flash \
+    --packing true \
+    --logging_steps 1
+"
+echo $run_cmd
+eval $run_cmd
